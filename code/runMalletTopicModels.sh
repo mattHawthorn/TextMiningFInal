0321d283d0cad1f16767897fd473fe7008e2a678
nm1291 <nikhil.mascarenhas@gmail.com>
2016-04-24 17:34:46 -0400

    Added bash script for topic models

---
+++ code/runMalletTopicModels.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#####################
+
+MALLETDIR="/home/nikhil/mallet/"
+MALLET="bin/mallet"
+INPUTDIR="/home/nikhil/Documents/TextMiningProject/TextMiningFinal/data/mallet/"
+OUTPUTDIR="/home/nikhil/Documents/TextMiningProject/TextMiningFinal/data/topics/"
+
+MALLETFILES=("20_newsgroups_complex.mallet" "20_newsgroups_simple.mallet")
+TOPICS=(5 10 20 40 80)
+
+OPTINT=10
+
+####################
+
+for FILE in "${MALLETFILES[@]}"
+do
+	for TOPIC in "${TOPICS[@]}"
+	do
+		echo "$MALLETDIR$MALLET train-topics --input $INPUTDIR$FILE --num-topics $TOPIC --output-topic-keys "$FILE.$TOPIC.topics_KEYS.txt" --output-doc-topics \"$FILE.$TOPIC.topics_COMPOSITION.txt\" --optimize-interval $OPTINT"		
+		$MALLETDIR$MALLET train-topics --input $INPUTDIR$FILE --num-topics $TOPIC --output-topic-keys "$FILE.$TOPIC.topics_KEYS.txt" --output-doc-topics "$FILE.$TOPIC.topics_COMPOSITION.txt" --optimize-interval $OPTINT
+		break
+	done
+done
+
+
+
+ 
+
