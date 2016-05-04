bab62d9159d5ac9e1b2dfe51b23ac29862cc05ea
nm1291 <nikhil.mascarenhas@gmail.com>
2016-05-04 17:10:07 -0400

    ran topics on reuters - 5,10,20,40,80,160

--- code/runMalletTopicModels.sh
+++ code/runMalletTopicModels.sh
@@ -7,8 +7,8 @@ MALLET="bin/mallet"
 INPUTDIR="/home/nikhil/Documents/TextMiningProject/TextMiningFinal/data/mallet/"
 OUTPUTDIR="/home/nikhil/Documents/TextMiningProject/TextMiningFinal/data/topics/"
 
-MALLETFILES=("20_newsgroups_complex.mallet" "20_newsgroups_simple.mallet")
-TOPICS=(5 10 20 40 80)
+MALLETFILES=("reuters_simple.mallet")
+TOPICS=(5 10 20 40 80 160)
 
 OPTINT=10
 
